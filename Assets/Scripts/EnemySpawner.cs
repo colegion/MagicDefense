ff3e3eae7f4e669857fa1662ffa640300480151d
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-12-05 15:02:34 +0300

    Randomized apwn point on enemies when fetched from the pool

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Helpers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -49,14 +50,50 @@ public class EnemySpawner : MonoBehaviour
         {
             var enemy = GameController.Instance.GetEnemy();
             var config = enemies.Find(e => e.enemyType == type);
+            Vector3 spawnPosition = GetRandomSpawnPosition();
+            enemy.transform.position = spawnPosition;
             enemy.ConfigureSelf(config);
             enemy.Move(GameController.Instance.GetTowerTransform());
             yield return new WaitForSeconds(config.enemySettings.currentSpawnCooldown);
-            
         }
         
         StopSpawning();
     }
+    
+    private Vector3 GetRandomSpawnPosition()
+    {
+        Camera mainCamera = Camera.main;
+        if (mainCamera == null)
+            throw new Exception("Main Camera not found!");
+        
+        Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
+        Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane));
+
+        float screenLeft = bottomLeft.x;
+        float screenRight = topRight.x;
+        float screenBottom = bottomLeft.z;
+        float screenTop = topRight.z;
+        
+        float offset = 2f;
+
+        // Randomly choose an edge: 0 = left, 1 = right, 2 = top, 3 = bottom
+        int edge = Random.Range(0, 4);
+
+        switch (edge)
+        {
+            case 0: // Left
+                return new Vector3(screenLeft - offset, 0, Random.Range(screenBottom, screenTop));
+            case 1: // Right
+                return new Vector3(screenRight + offset, 0, Random.Range(screenBottom, screenTop));
+            case 2: // Top
+                return new Vector3(Random.Range(screenLeft, screenRight), 0, screenTop + offset);
+            case 3: // Bottom
+                return new Vector3(Random.Range(screenLeft, screenRight), 0, screenBottom - offset);
+            default:
+                throw new Exception("Invalid edge case.");
+        }
+    }
+
 
     private void StopSpawning()
     {
